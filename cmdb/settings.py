9af1a0676c287a8b1c45468920db28acf1d0bd1f
h0neyBadger <h0neyb4dgerdc@gmail.com>
2017-09-03 09:59:35 +0200

    Updates pagintation class to use LimitOffsetPagination

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -53,10 +53,11 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser',
     ],
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
 
 ROOT_URLCONF = 'cmdb.urls'
